63fdaf00bb45a948a6fea305bc7ec08c0fb5b847
kami234-tech <marekami234@gmail.com>
2025-05-22 08:07:15 +0300

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,7 +1,13 @@
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials
+import os
+import json
 
-cred = credentials.Certificate("budgetti-admin-sdk.json")
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
+# Read JSON from environment variable
+firebase_creds = os.environ.get("FIREBASE_ADMIN_CREDS")
+if firebase_creds:
+    cred_dict = json.loads(firebase_creds)
+    cred = credentials.Certificate(cred_dict)
+    firebase_admin.initialize_app(cred)
+else:
+    raise RuntimeError("FIREBASE_ADMIN_CREDS environment variable not set.")
